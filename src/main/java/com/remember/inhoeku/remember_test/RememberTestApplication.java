ff1c57526b7559480fd74ab6ece330b45d2f4e67
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-08 21:06:24 +0900

    first commit

---
+++ src/main/java/com/remember/inhoeku/remember_test/RememberTestApplication.java
@@ -0,0 +1,13 @@
+package com.remember.inhoeku.remember_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberTestApplication.class, args);
+	}
+
+}
