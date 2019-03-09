263a804cc8995bf5bdb2b62eabbda35196d13225
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-10 01:14:52 +0900

    set time zone as Asia Seoul

--- src/main/java/com/remember/inhoeku/remember_test/RememberTestApplication.java
+++ src/main/java/com/remember/inhoeku/remember_test/RememberTestApplication.java
@@ -3,6 +3,9 @@ package com.remember.inhoeku.remember_test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class RememberTestApplication {
 
@@ -10,4 +13,8 @@ public class RememberTestApplication {
 		SpringApplication.run(RememberTestApplication.class, args);
 	}
 
+	@PostConstruct
+	public void initApplication() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
