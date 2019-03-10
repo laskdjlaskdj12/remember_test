f49dde60083b957e1ffaffa62393c721d55dd7d8
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-10 13:24:33 +0900

    refactor Class Capeteal Letter

--- src/test/java/com/remember/inhoeku/remember_test/dao/ClientAccountDAOTest.java
+++ src/test/java/com/remember/inhoeku/remember_test/dao/ClientAccountDAOTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class clientAccountDAOTest {
+public class ClientAccountDAOTest {
 	@Autowired
 	ClientAccountDAO clientAccountDAO;
 
--- src/test/java/com/remember/inhoeku/remember_test/dao/DriverAccountDAOTest.java
+++ src/test/java/com/remember/inhoeku/remember_test/dao/DriverAccountDAOTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class driverAccountDAOTest {
+public class DriverAccountDAOTest {
 	@Autowired
 	DriverAccountDAO driverAccountDAO;
 
