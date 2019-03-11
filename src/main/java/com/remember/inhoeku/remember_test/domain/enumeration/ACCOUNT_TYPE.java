663bfb4d60c9c34c4ba3ba521c7ac927b9e906be
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-11 18:42:28 +0900

    add int to ACCOUNT_TYPE static cast method

--- src/main/java/com/remember/inhoeku/remember_test/domain/enumeration/ACCOUNT_TYPE.java
+++ src/main/java/com/remember/inhoeku/remember_test/domain/enumeration/ACCOUNT_TYPE.java
@@ -12,4 +12,15 @@ public enum ACCOUNT_TYPE {
 	ACCOUNT_TYPE(int value){
 		this.value = value;
 	}
+
+	public static ACCOUNT_TYPE fromValue(int value){
+		switch(value){
+			case 0:
+				return ACCOUNT_TYPE.PASSENGER;
+			case 1:
+				return ACCOUNT_TYPE.DRIVER;
+			default:
+				return null;
+		}
+	}
 }
