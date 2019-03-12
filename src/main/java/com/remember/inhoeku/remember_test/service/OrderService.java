42a4148c7eca56fcaa8523c777bdeec7c9f7c415
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-12 19:15:52 +0900

    refactor dispatch order to orderService

--- src/main/java/com/remember/inhoeku/remember_test/service/OrderService.java
+++ src/main/java/com/remember/inhoeku/remember_test/service/OrderService.java
@@ -1,11 +1,8 @@
 package com.remember.inhoeku.remember_test.service;
 
-import com.remember.inhoeku.remember_test.dao.DispatchRequestDAO;
 import com.remember.inhoeku.remember_test.dao.OrderDAO;
 import com.remember.inhoeku.remember_test.domain.dto.OrderDTO;
-import com.remember.inhoeku.remember_test.domain.enumeration.ORDER_STATE;
 import com.remember.inhoeku.remember_test.domain.error.BusinessException;
-import com.remember.inhoeku.remember_test.domain.vo.DispatchRequestVO;
 import com.remember.inhoeku.remember_test.domain.vo.OrderVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,7 +19,7 @@ public class OrderService {
 	OrderDAO orderDAO;
 
 	@Autowired
-	DispatchRequestDAO dispatchRequestDAO;
+	DispatchService dispatchService;
 
 	public OrderVO order(OrderDTO orderDTO) {
 		String depatureString = orderDTO.getDeparture();
@@ -46,26 +43,7 @@ public class OrderService {
 
 		OrderVO orderVO = orderVOList.get(0);
 
-		//DispatchRequstList에 넣음
-		DispatchRequestVO dispatchRequestVO = makeDispatchRequest(orderVO);
-		int insertComplete = dispatchRequestDAO.insertDispathRequest(dispatchRequestVO);
-
-		if(isDispatchRequestInsertSuccess(insertComplete)){
-			throw new RuntimeException("insert dispatch Request Fail");
-		}
-
-		return orderVO;
-	}
-
-	private boolean isDispatchRequestInsertSuccess(int insertComplete) {
-		return insertComplete > 0;
-	}
-
-	private DispatchRequestVO makeDispatchRequest(OrderVO orderVO) {
-		DispatchRequestVO dispatchRequestVO = new DispatchRequestVO();
-		dispatchRequestVO.setOrderPK(orderVO.getPK());
-		dispatchRequestVO.setOrderState(ORDER_STATE.READY);
-		return dispatchRequestVO;
+		return dispatchService.request(orderVO);
 	}
 
 	private String dateToString(Date date){
